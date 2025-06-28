4c37fcdbb789afcb62f3641745e6ae74557a9569
Yash Gautam <kryash720@gmail.com>
2025-06-28 16:32:37 +0530

    Initial commit for SkillMerge Angular frontend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', loadChildren: () => 
+    import('./modules/auth/auth.module').then(m => m.AuthModule) 
+  }, 
+  { path: 'dashboard', loadChildren: () => 
+    import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
